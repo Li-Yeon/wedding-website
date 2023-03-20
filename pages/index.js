3ac8667376727b3b0189c51c89760c2e17c08b62
Li Yeon <mekaleon27@gmail.com>
2023-03-20 20:02:41 +0800

    Initialize

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (  
+    <div>
+        <h1 className='font-Poppins text-black'>NextJS Tailwind Boilerplate</h1>
+    </div>
+  )
+}
